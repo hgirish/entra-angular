5180aebea00932577fe9d7aab9d204029a0a3c92
hgirish <girish@post.com>
2024-08-19 21:58:16 -0700

    auth settings in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  auth: {
+    clientId: 'application client id',
+    authority: 'https://login.microsoftonline.com/<tenantid>',
+    redirectUri: 'http://localhost:4200/',
+  }
+};
